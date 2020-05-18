4fcdec05bb0601ffe25a7b5b08b67a783a138f7b
Omkar Shidore <35575009+OmkarShidore@users.noreply.github.com>
2020-05-18 20:36:31 +0530

    Add files via upload

--- darknet_yolo_v3.py
+++ darknet_yolo_v3.py
@@ -2,6 +2,59 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F 
 
+
+#Please check yolov3.cfg file for configuration of YOLO V3
+'''
+There are 5 types of layers in YOLO V3
+1. Convolution
+
+2. Shortcut: It's a skip connection like the one used in ResNet, where the
+    output of the previous layer and and previous 3rd layer backwards from shortcut layer.
+    We concate them and squeeze them through linear activation function
+    #Example:   [shortcut]
+                from=-3  
+                activation=linear  
+
+3. Upsample: Upsamle is feature map of previous layer with a stride, which helps 
+    increasing depth using bilinear upsampling.
+    #Example:    [upsample]
+                stride=2
+
+4. Route: It has an attribute layers which can have either one, or two values.
+    #Example:   [route]
+                layers = -4
+    When layers attribute has only one value, it outputs the feature maps of the 
+    layer indexed by the value. In our example, it is -4, so the layer will output 
+    feature map from the 4th layer backwards from the Route layer.
+
+    #Example:   [route]
+                layers = -1, 61
+    When layers has two values, it returns the concatenated feature maps of the layers
+     indexed by it's values. In our example it is -1, 61, and the layer will output 
+     feature maps from the previous layer (-1) and the 61st layer, concatenated along 
+     the depth dimension.      
+
+5. YOLO
+    #Example:
+                [yolo]
+                mask = 0,1,2
+                anchors = 10,13,  16,30,  33,23,  30,61,  62,45,  59,119,  116,90,  156,198,  373,326
+                classes=80  
+                num=9
+                jitter=.3
+                ignore_thresh = .5
+                truth_thresh = 1
+                random      
+    YOLO layer corresponds to the Detection layer.
+    The anchors describes 9 'anchors', but only the anchors which are indexed by 
+    'attributes' of the 'mask' tag are used. Here, the value of 'mask' is 0,1,2, which means 
+    the first, second and third anchors are used as each cell of the detection layer 
+    predicts 3 boxes. In total, we have detection layers at 3 scales, 
+    making up for a total of 9 anchors.
+'''
+
+
+
 #wrapper
 #Convolution with batch noramlization
 class BasicConv2d_bn(nn.Module):
@@ -186,7 +239,11 @@ class YoloV3Model(nn.Module):
         
         
 
-    def forward(self,x):    
+    def forward(self,x):
+        '''
+        We use shortcut layer here, a short layer is skip connection like the one used is ResNet
+        '''
+
         #3x416x416 x.shape
         x=self.Conv2d_1(x) 
         short=x=self.Conv2d_2(x)   #downsample  
